5e2e1a2244bbb623a181aec1144e8539fc61fac4
lizhiwei <wei2006004@foxmail.com>
2016-12-16 14:55:38 +0800

    init demo

---
+++ defthttp-core/src/main/kotlin/com/deft/http/DHttp.kt
@@ -0,0 +1,10 @@
+package com.deft.http
+
+/**
+ * Created by Administrator on 2016/12/16.
+ */
+
+fun boot() {
+    println("hello http")
+}
+
---
+++ defthttp-demo/src/main/kotlin/Demo.kt
@@ -0,0 +1,8 @@
+import com.deft.http.boot
+
+/**
+ * Created by Administrator on 2016/12/16.
+ */
+fun main(args:Array<String>){
+    boot()
+}
